15e14925b58acc9a3aa9d4151ea06966d248772e
BeiMingYouYu666 <956898043@qq.com>
2018-09-05 23:40:40 +0800

    update

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -1,6 +1,8 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import sun.security.util.Password;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +16,21 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    int checkUsername(@Param("username") String username);
+
+    int checkEmail(@Param("email") String email);
+
+    User selectLogin(@Param("username")String username,@Param("password")String password);
+
+    String selectQuestionByUsername(@Param("username") String username);
+
+    int checkAnswer(@Param("username") String username,@Param("question")String question,@Param("answer")String answer);
+
+    int updatePasswordByUsername(@Param("username")String username,@Param("password")String password);
+
+    int checkPassword(@Param("password")String password,@Param("id")Integer id);
+
+    int checkEmailByUserId(@Param(value="email")String email,@Param(value="userId")Integer userId);
+
 }
